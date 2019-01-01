9051db18acf17a7e974d6e206952db1afde19e17
hitaspdotnet <mostafa.foruji@gmail.com>
2019-01-01 23:28:07 +0330

    removed urlRecord lookup service

--- modules/common/src/Hitasp.HitCommon.Domain/Hitasp/HitCommon/Helpers/CurrencyHelper.cs
+++ modules/common/src/Hitasp.HitCommon.Domain/Hitasp/HitCommon/Helpers/CurrencyHelper.cs
@@ -5,11 +5,13 @@ namespace Hitasp.HitCommon.Helpers
 {
     public static class CurrencyHelper
     {
-        private static readonly List<string> ZeroDecimalCurrencies = new List<string> { "BIF", "DJF", "JPY", "KRW", "PYG", "VND", "XAF", "XPF", "CLP", "GNF", "KMF", "MGA", "RWF", "VUV", "XOF" };
+        private static readonly List<string> ZeroDecimalCurrencies = new List<string>
+            {"BIF", "DJF", "JPY", "KRW", "PYG", "VND", "XAF", "XPF", "CLP", "GNF", "KMF", "MGA", "RWF", "VUV", "XOF"};
 
         public static bool IsZeroDecimalCurrencies()
         {
             var regionInfo = new RegionInfo(CultureInfo.CurrentCulture.LCID);
+
             return ZeroDecimalCurrencies.Contains(regionInfo.ISOCurrencySymbol);
         }
     }
---
+++ modules/common/src/Hitasp.HitCommon.Domain/Hitasp/HitCommon/Models/Content.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Hitasp.HitCommon.Seo;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Hitasp.HitCommon.Models
+{
+    public abstract class Content : FullAuditedAggregateRoot<Guid>, ISlugSupported
+    {
+        public string Name { get; protected set; }
+
+        public string Slug { get; protected set; }
+
+        public string MetaTitle { get; protected set; }
+
+        public string MetaKeywords { get; protected set; }
+
+        public string MetaDescription { get; protected set; }
+
+        public bool IsPublished { get; protected set; }
+
+        public DateTimeOffset? PublishedOn { get; protected set; }
+
+        protected virtual void Publish()
+        {
+            IsPublished = true;
+            PublishedOn = DateTimeOffset.Now;
+        }
+
+        public virtual void SetName([NotNull] string name)
+        {
+            Name = Check.NotNullOrWhiteSpace(name, nameof(name));
+        }
+
+        public virtual void SetSlug([NotNull] string slug)
+        {
+            Slug = Check.NotNullOrWhiteSpace(slug, nameof(slug));
+        }
+        
+        public virtual void SetMetaData(string metaTitle, string metaKeywords, string metaDescription)
+        {
+            MetaTitle = metaTitle;
+            MetaKeywords = metaKeywords;
+            MetaDescription = metaDescription;
+        }
+    }
+}
