00f1e07aa0ed9c4cfc3817090ac731a006ee7d1f
hitaspdotnet <mostafa.foruji@gmail.com>
2019-01-14 23:30:03 +0330

    small change

--- modules/common/src/Hitasp.HitCommon.Domain/Hitasp/HitCommon/Contents/IContentBaseRepository.cs
+++ modules/common/src/Hitasp.HitCommon.Domain/Hitasp/HitCommon/Contents/IContentBaseRepository.cs
@@ -5,7 +5,7 @@ using Volo.Abp.Domain.Repositories;
 
 namespace Hitasp.HitCommon.Contents
 {
-    public interface IContentBaseRepository<TContent> : IBasicRepository<TContent, Guid>
+    public interface IContentBaseRepository<TContent> : IRepository<TContent, Guid>
         where TContent : ContentBase
     {
         Task<TContent> FindByNameAsync(string name, CancellationToken cancellationToken = default);
