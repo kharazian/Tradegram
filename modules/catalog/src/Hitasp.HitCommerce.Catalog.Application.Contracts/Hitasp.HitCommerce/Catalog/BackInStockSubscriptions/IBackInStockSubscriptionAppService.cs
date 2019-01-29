4c381bd94ca5fedde8026691ad84697416b726ae
hitaspdotnet <mostafa.foruji@gmail.com>
2019-01-29 14:39:33 +0330

    BackInStockSubscription pagedList method

--- modules/catalog/src/Hitasp.HitCommerce.Catalog.Application.Contracts/Hitasp.HitCommerce/Catalog/BackInStockSubscriptions/IBackInStockSubscriptionAppService.cs
+++ modules/catalog/src/Hitasp.HitCommerce.Catalog.Application.Contracts/Hitasp.HitCommerce/Catalog/BackInStockSubscriptions/IBackInStockSubscriptionAppService.cs
@@ -2,24 +2,18 @@ using System;
 using System.Threading.Tasks;
 using Hitasp.HitCommerce.Catalog.BackInStockSubscriptions.Dtos;
 using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
 
 namespace Hitasp.HitCommerce.Catalog.BackInStockSubscriptions
 {
-    public interface IBackInStockSubscriptionAppService
+    public interface IBackInStockSubscriptionAppService : IAsyncCrudAppService<BackInStockSubscriptionDto, Guid,
+        BackInStockSubscriptionGetListInput, BackInStockSubscriptionCreateDto, BackInStockSubscriptionUpdateDto>
     {
-        Task DeleteAsync(Guid subscriptionId);
-        
         Task<PagedResultDto<BackInStockSubscriptionDto>> GetListByCustomerIdAsync(BackInStockSubscriptionGetListByCustomerIdInput input);
         
         Task<PagedResultDto<BackInStockSubscriptionDto>> GetListByProductIdAsync(BackInStockSubscriptionGetListByProductIdInput input);
 
         Task<BackInStockSubscriptionDto> FindAsync(Guid customerId, Guid productId);
-
-        Task<BackInStockSubscriptionDto> GetAsync(Guid subscriptionId);
-
-        Task<BackInStockSubscriptionDto> CreateAsync(BackInStockSubscriptionCreateDto subscription);
-
-        Task<BackInStockSubscriptionDto> UpdateAsync(BackInStockSubscriptionUpdateDto subscription);
 
         Task<int> SendNotificationsToSubscribersAsync(Guid productId);
     }
