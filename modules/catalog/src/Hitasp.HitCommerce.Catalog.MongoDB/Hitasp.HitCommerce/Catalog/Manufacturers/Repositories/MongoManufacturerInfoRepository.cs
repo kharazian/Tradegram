187c29a39c3186631d12702e3542e7c2af0fde5b
hitaspdotnet <mostafa.foruji@gmail.com>
2019-01-31 15:03:36 +0330

    manufacturers

--- modules/catalog/src/Hitasp.HitCommerce.Catalog.MongoDB/Hitasp.HitCommerce/Catalog/Manufacturers/Repositories/MongoManufacturerInfoRepository.cs
+++ modules/catalog/src/Hitasp.HitCommerce.Catalog.MongoDB/Hitasp.HitCommerce/Catalog/Manufacturers/Repositories/MongoManufacturerInfoRepository.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Hitasp.HitCommerce.Catalog.Manufacturers.Entities;
 using Hitasp.HitCommerce.Catalog.MongoDB;
+using MongoDB.Driver.Linq;
 using Volo.Abp.Domain.Repositories.MongoDB;
 using Volo.Abp.MongoDB;
 
@@ -11,6 +14,20 @@ namespace Hitasp.HitCommerce.Catalog.Manufacturers.Repositories
     {
         public MongoManufacturerInfoRepository(IMongoDbContextProvider<ICatalogMongoDbContext> dbContextProvider) : base(dbContextProvider)
         {
+        }
+        
+        public async Task<ManufacturerInfo> FindByNameAsync(string name,
+            CancellationToken cancellationToken = default)
+        {
+            return await GetMongoQueryable().FirstOrDefaultAsync(x => x.Name == name,
+                GetCancellationToken(cancellationToken));
+        }
+
+        public async Task<ManufacturerInfo> FindByTitleAsync(string title,
+            CancellationToken cancellationToken = default)
+        {
+            return await GetMongoQueryable().FirstOrDefaultAsync(x => x.Title == title,
+                GetCancellationToken(cancellationToken));
         }
     }
 }
--- modules/catalog/src/Hitasp.HitCommerce.Catalog.MongoDB/Hitasp.HitCommerce/Catalog/Manufacturers/Repositories/MongoManufacturerRepository.cs
+++ modules/catalog/src/Hitasp.HitCommerce.Catalog.MongoDB/Hitasp.HitCommerce/Catalog/Manufacturers/Repositories/MongoManufacturerRepository.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Threading;
-using System.Threading.Tasks;
 using Hitasp.HitCommerce.Catalog.Manufacturers.Aggregates;
 using Hitasp.HitCommerce.Catalog.MongoDB;
-using MongoDB.Driver.Linq;
 using Volo.Abp.Domain.Repositories.MongoDB;
 using Volo.Abp.MongoDB;
 
@@ -14,20 +11,6 @@ namespace Hitasp.HitCommerce.Catalog.Manufacturers.Repositories
     {
         public MongoManufacturerRepository(IMongoDbContextProvider<ICatalogMongoDbContext> dbContextProvider) : base(dbContextProvider)
         {
-        }
-
-        public async Task<Manufacturer> FindByNameAsync(string name,
-            CancellationToken cancellationToken = default)
-        {
-            return await GetMongoQueryable().FirstOrDefaultAsync(x => x.ManufacturerInfo.Name == name,
-                GetCancellationToken(cancellationToken));
-        }
-
-        public async Task<Manufacturer> FindByTitleAsync(string title,
-            CancellationToken cancellationToken = default)
-        {
-            return await GetMongoQueryable().FirstOrDefaultAsync(x => x.ManufacturerInfo.Title == title,
-                GetCancellationToken(cancellationToken));
         }
     }
 }
